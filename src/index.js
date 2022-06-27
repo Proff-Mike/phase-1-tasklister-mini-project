30d75161832fa92fd48908e343823b10fe9f978d
Michael Okoe <michael.okoejr@gmail.com>
2022-06-27 12:12:15 +0000

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.addEventListener("DOMContentLoaded", () => {
+  
+  const form = document.getElementById('#create-task-form');
+    form.addEventListener('submit', (event) => event.preventDefault())
+  
+    let userInput = document.getElementById('#new-task-description').value 
+    console.log(userInput);
+
+    let li = document.createElement('li');
+    li.append(userInput);
+    document.querySelector('tasks').appendChild(li);
+
 });
+
